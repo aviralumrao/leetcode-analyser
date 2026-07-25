e68fa1713bd931174f733d035dc3cd2a7d1850d5
aviralumrao <aviral0419@gmail.com>
2026-07-25 19:40:05 +0530

    structured the page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,12 @@ export default function Home() {
 
       <main className="mx-auto flex max-w-6xl flex-col gap-4 px-4 py-6 sm:px-6 lg:px-8">
         <SearchBar />
+        <ProfileCard />
 
-        <div className="grid gap-4 md:grid-cols-[1.2fr_0.8fr]">
-          <ProfileCard />
+        <div className="mt-8 grid gap-6 md:grid-cols-2 lg:grid-cols-4">
+          <StatsCard />
+          <StatsCard />
+          <StatsCard />
           <StatsCard />
         </div>
       </main>
