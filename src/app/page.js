006ca021d364244fbc3bf74c12cdc24921da3fca
aviralumrao <aviral0419@gmail.com>
2026-07-25 22:39:28 +0530

    set search value to username, searchhandle , used useState from react to manage search

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,26 @@ import SearchBar from "@/components/SearchBar";
 import ProfileCard from "@/components/ProfileCard";
 import StatsCard from "@/components/StatsCard";
 import Footer from "@/components/Footer";
+import { useState } from "react";
 
 export default function Home() {
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function handleSearch() {
+    console.log(username);
+  }
   return (
     <>
       <Navbar />
 
       <main className="mx-auto flex max-w-6xl flex-col gap-4 px-4 py-6 sm:px-6 lg:px-8">
-        <SearchBar />
+        <SearchBar
+          username={username}
+          setUsername={setUsername}
+          handleSearch={handleSearch}
+          loading={loading}
+        />
         <ProfileCard />
 
         <div className="mt-8 grid gap-6 md:grid-cols-2 lg:grid-cols-4">
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,28 @@
 "use client";
 import { Search } from "lucide-react";
 
-export default function SearchBar() {
+export default function SearchBar({
+  username,
+  setUsername,
+  handleSearch,
+  loading,
+}) {
   return (
     <div className="flex justify-center py-1 gap-2">
       <input
         type="text"
-        placeholder="Enter LeetCode username..."
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Enter LeetCode username"
         className="w-2xl border rounded-full px-3 py-2"
       />
 
-      <button className="flex items-center gap-2 rounded bg-gray-800 px-3 py-2 text-white">
-        <Search size={16} />
-        <span>Search</span>
+      <button
+        onClick={handleSearch}
+        disabled={loading}
+        className="flex items-center gap-2 rounded bg-gray-800 px-3 py-2 text-white"
+      >
+        {loading ? "Searching..." : "Search"}
       </button>
     </div>
   );
