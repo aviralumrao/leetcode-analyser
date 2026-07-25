0f93022083d6912cd1d157050bf56a0b951d97dd
aviralumrao <aviral0419@gmail.com>
2026-07-25 17:02:21 +0530

    styled the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,11 +3,16 @@ import { Search } from "lucide-react";
 
 export default function SearchBar() {
   return (
-    <div>
-      <input type="text" placeholder="Enter LeetCode username..." />
+    <div className="flex justify-center py-1 gap-2">
+      <input
+        type="text"
+        placeholder="Enter LeetCode username..."
+        className="w-2xl border rounded-full px-3 py-2"
+      />
 
-      <button>
-        <Search /> Search
+      <button className="flex items-center gap-2 rounded bg-gray-800 px-3 py-2 text-white">
+        <Search size={16} />
+        <span>Search</span>
       </button>
     </div>
   );
